ee4569573e75e73069f99f756fdc02b990cd098d
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-12-25 16:43:29 +0530

    Initial commit

---
+++ src/DoxygenCommandAspNet/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoxygenCommandAspNet.Controllers
+{
+    /// <summary>
+    /// Home controller 
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Index action
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// About us action
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Contact action
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Error
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
